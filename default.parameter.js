2fee2fdd91fd8da48e5f381cf9bed48ab786467f
shafayet-hossain <shafayethossaing4609@gmail.com>
2021-08-22 23:34:17 +0600

    default parameter

---
+++ default.parameter.js
@@ -0,0 +1,8 @@
+// Default Parameter
+function sum(num1, num2 = 0, num3 = 0, num4 = 0, num5 = 10){
+    const result = num1 + num2 + num3 + num4 + num5
+    return result
+}
+console.log(sum(10, 20, 10, 20));
+
+
