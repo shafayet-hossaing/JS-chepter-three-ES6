83fb31d49daf549d5dab095c0fbb26109f9df3f6
shafayet-hossain <shafayethossaing4609@gmail.com>
2021-08-23 19:42:04 +0600

    destructuring

--- destructuring.js
+++ destructuring.js
@@ -2,3 +2,42 @@
 const Jon = ["Jon", "21", "America"]
 const [names, age, country] = Jon
 console.log(country);
+
+
+// Normal Object Destructuring
+const mark = {
+    name : "Mark",
+    age : 23,
+    profession : "Student"
+}
+
+const {name, age, profession} = mark
+console.log(name,age,profession);
+
+
+// Normal Object Destructuring And Setting The New Names Of Keys
+const mark = {
+    name : "Mark",
+    age : 23,
+    profession : "Student"
+}
+
+const {name:studentNames, age:studentAge, profession:studentProfession} = mark
+console.log(studentNames);
+
+
+
+
+// Nested Object Destructuring
+const mark = {
+    nameFinding: {
+        newNameFinding: {
+            name : "Mark",
+            age : 23,
+            profession : "Student"
+        }
+    }
+}
+
+const {nameFinding: {newNameFinding: {name:names}}} = mark
+console.log(names);
