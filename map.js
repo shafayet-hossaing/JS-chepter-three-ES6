4c2971be37f8748308b1d89a1c96817a3b1bdf7c
shafayet-hossain <shafayethossaing4609@gmail.com>
2021-08-24 10:58:59 +0600

    nothing special

--- map.js
+++ map.js
@@ -1,16 +1,25 @@
 const numbers = [12, 13, 15, 56, 58]
 
 // Just Map()
-// let doubleIt = numbers.map(number => number * 2)
-// console.log(doubleIt);
+let doubleIt = numbers.map(number => number * 2)
+console.log(doubleIt);
 
 
 // Map With Arrow Function And IIFE As Well
-// const doubleIt = ((numbers) => {
-//     const double = numbers.map(number => number * 2)
-//     console.log(double);
-// })(numbers)
+const doubleIt = ((numbers) => {
+    const double = numbers.map(number => number * 2)
+    console.log(double);
+})(numbers)
 
 
 // Map With Arrow Function And IIFE As Well But In The Same Line
 const doubleIt = (numbers => console.log(numbers.map(number => number * 2)))(numbers);
+
+
+
+// Object 
+const products = [
+    {name: "Smart Watch", price: 1000},
+    {name: "Samsung", price: 2000}
+]
+const productNames = (product => console.log(product.map(product => product.name)))(products);
